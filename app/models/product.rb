35d31e0598a621502d31679701537cbcdbeca832
Dalal Alnasrallah <dalalalnasrallah@hotmail.com>
2015-04-11 15:12:41 +0100

    Search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ActiveRecord::Base
   has_many :order_items
 
-  default_scope { where(active: true) }
+  has_many :reviews 
+
+    default_scope { where(active: true) }
+
+  def self.search(manufacturer, cateogory)
+  	if manufacturer.present? && cateogory.present?	
+	    where("manufacturer like ?", "%#{manufacturer}%") && where( "cateogory like ?", "%#{cateogory}%") 
+	elsif manufacturer.present?
+			where("manufacturer like ?", "%#{manufacturer}%")
+	elsif cateogory.present?
+			where( "cateogory like ?", "%#{cateogory}%")
+  	end
+  end
 end
